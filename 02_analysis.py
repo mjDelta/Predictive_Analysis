b31581f859b1007241507c5d620c740a1d8680cf
zzmmjj <1363177498@qq.com>
2017-06-07 22:56:15 +0800

    利用平行坐标图对多属性问题分析。

--- 02_analysis.py
+++ 02_analysis.py
@@ -5,8 +5,7 @@ Created on Wed Jun 07 22:36:24 2017
 @author: ZMJ
 """
 import pandas as pd
-from pandas import DataFrame
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plot
 #pandas直接读取网络数据
 target_url=("https://archive.ics.uci.edu/ml/machine-learning-"\
   "databases/undocumented/connectionist-bench/sonar/sonar.all-data")
@@ -15,11 +14,19 @@ pd.read_csv()：header=None表示不要将数据第一行作为列标签；prefi
 """
 rocks_mines=pd.read_csv(target_url,header=None,prefix="V")
 
-#查看头五条尾五条数据
-print rocks_mines.head()
-print rocks_mines.tail()
+"""
+dataframe中定位到元素用：iat
+"""
+for i in range(208):
+  #根据标签指定两种不同的颜色
 
-#查看描述统计信息
-summary=rocks_mines.describe()
-print summary
+  if rocks_mines.iat[i,60]=="M":
+    c="red"
+  else:
+    c="blue"
+  dataRow=rocks_mines.iloc[i,0:60]
+  dataRow.plot(color=c)
 
+plot.xlabel("Attribute Index")
+plot.ylabel("Attribute Values")
+plot.show()
